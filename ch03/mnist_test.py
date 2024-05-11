010d0d3b51c693d7dd5d1a4f66d362c4155ead89
michang <minseok128128@gmail.com>
2024-05-11 16:12:33 +0900

    Update mnist_test.py : 미리 학습된 network 가중치 불러와서 이를 기반으로 test 진행

--- ch03/mnist_test.py
+++ ch03/mnist_test.py
@@ -1,10 +1,46 @@
-import sys, os
+import sys, os, pickle
+import numpy as np
+import matplotlib.pyplot as plt
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+def softmax(x):
+    c = np.max(x)
+    exp_a = np.exp(x - c)
+    return (exp_a / np.sum(exp_a))
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = x @ W1 + b1
+    z1 = sigmoid(a1)
+    a2 = z1 @ W2 + b2
+    z2 = sigmoid(a2)
+    a3 = z2 @ W3 + b3
+    y = softmax(a3)
+    return y
+
+network = init_network()
+x, t = get_data()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy: ", float(accuracy_cnt / len(x)))
